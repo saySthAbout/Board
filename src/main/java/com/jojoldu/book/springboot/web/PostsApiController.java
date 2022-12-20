622da58c8adecd4a044d5e12b9a4b4e4f4f0239a
saySthAbout <saysthabout@gmail.com>
2022-12-20 12:04:16 +0900

    Travis CI 연동 테스트

--- src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
+++ src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
@@ -17,7 +17,7 @@ public class PostsApiController {
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
     }
-
+ 
     @PutMapping("/api/v1/posts/{id}")
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
         return postsService.update(id, requestDto);
