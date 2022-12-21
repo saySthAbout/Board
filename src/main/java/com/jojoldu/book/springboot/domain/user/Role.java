124407badefccf5dd8fb223b0bb4c4d9ae516623
saySthAbout <saysthabout@gmail.com>
2022-12-21 19:48:52 +0900

    test

--- src/main/java/com/jojoldu/book/springboot/domain/user/Role.java
+++ src/main/java/com/jojoldu/book/springboot/domain/user/Role.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.springboot.domain.user;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -8,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 public enum Role {
 
     GUEST("ROLE_GUEST", "손님"),
+
+    @Builder.Default
     USER("ROLE_USER", "일반 사용자");
 
     private final String key;
